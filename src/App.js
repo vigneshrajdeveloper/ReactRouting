27baeea947c06a35f950fcf83af743386970c5de
Vigneshraj&123 <vigneshdeva31@gmail.com>
2024-07-08 16:55:07 +0530

    fix: Routed all component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import Home  from './components/homepage';
-import Dashboard from './components/dashboard';
-import About from './components/about';
+
 
 function App() {
+
   return (
     <div className="App">
       
-      
       <Home></Home>
+      
       
     </div>
   );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,21 +3,35 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import { BrowserRouter as Router , Route,Routes } from "react-router-dom";
+
+import About from "./about";
+import Dashboard from "./dashboard";
+import App from "../App";
+
 export default function Header(){
 
     return(
 
         <>
+        <Router>
         <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home"><b>Boeing</b></Navbar.Brand>
+          <Navbar.Brand href="/"><b>Boeing</b></Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Dashboard</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/dash">Dashboard</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <Routes>
+        <Route path='/' exact element={<App/>}></Route>
+        <Route path='/about' exact element={<About/>}></Route>
+        <Route path='/dash'  exact element={<Dashboard/>}></Route>
+      </Routes>
+      </Router>
         </>
     )
 }
