d99a3a492d8dabecf3b6acb77aa21eba3c87b794
Vigneshraj&123 <vigneshdeva31@gmail.com>
2024-07-08 15:44:01 +0530

    fix : components created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import Home  from './components/homepage';
+import Dashboard from './components/dashboard';
+import About from './components/about';
 
 function App() {
   return (
@@ -11,15 +14,17 @@ function App() {
       
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Boeing</Navbar.Brand>
+          <Navbar.Brand href="#home"><b>Boeing</b></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Dashboard</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      
+      <Home></Home>
+      <Dashboard></Dashboard>
+      <About></About>
     </div>
   );
 }
