275943668fb8300ec21c28cf2512d7abfaa46112
Vigneshraj&123 <vigneshdeva31@gmail.com>
2024-07-08 15:16:05 +0530

    fix: nav bar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Developer branch</h1>
+      
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Boeing</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      
     </div>
   );
 }
